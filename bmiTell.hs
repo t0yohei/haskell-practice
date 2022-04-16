56c3fbc6354d9def6d5358cebaf3848df7dcd3f3
t0yohei <inuneko690121@gmail.co>
2022-04-16 17:16:52 +0900

    update guards with where

--- bmiTell.hs
+++ bmiTell.hs
@@ -1,10 +1,15 @@
--- guards
-bmiTell :: Double -> String
-bmiTell bmi
-  | bmi <= 18.5 = "You're underweight"
-  | bmi <= 25.0 = "You're Normal"
-  | bmi <= 30.0 = "You're overwaight"
+-- guards with where
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+  | bmi <= skinny = "You're underweight"
+  | bmi <= normal = "You're Normal"
+  | bmi <= fat = "You're overwaight"
   | otherwise = "Too match waight"
+  where
+    bmi = weight / height ^ 2
+    skinny = 18.5
+    normal = 25.0
+    fat = 30.0
 
 -- call sample
--- bmiTell 22.5
+-- bmiTell 65.5 1.75
